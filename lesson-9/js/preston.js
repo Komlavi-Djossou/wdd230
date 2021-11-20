4e07939ba5553fa3897361695c49e1a30626d83f
Komlavi-Djossou <andreygyos@outlook.fr>
2021-11-20 20:02:59 +0000

    home page

---
+++ lesson-9/js/preston.js
@@ -0,0 +1,34 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thusrday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let date = dayName + "," + monthName + " " + d.getDate() + ", " + year;
+let hours = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+let fullDate = date+' '+hours;
+document.getElementById("currentdate").textContent = fullDate;
+
+
